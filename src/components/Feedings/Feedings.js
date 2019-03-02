2233360c3538ed9a0f0afb7389641716f36f325d
Joe Hinkley <joehhinkley@gmail.com>
2019-03-02 09:03:40 -0800

    created feedings and foods components

---
+++ src/components/Feedings/Feedings.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Feedings extends Component {
+  render() {
+    return (
+      <div>
+        Feedings
+      </div>
+    )
+  }
+}
+
+export default Feedings
